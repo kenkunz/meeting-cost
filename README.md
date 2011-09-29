8fa72b3cb41936410d1e10d93a21750fe044d9d5
Ken Kunz <ken.kunz@vibes.com>
2011-09-28 23:23:50 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+= Meeting Cost Clock
+
+Expose the cost of meetings.
+
+== To Do
+
+* "End it" button
+* make it pretty
+* configuration (average salary, type of peep)
+* improve performance
+* "share"
+* "donate"
+
---
+++ javascripts/cost_clock.js
@@ -0,0 +1,65 @@
+$(function(){
+  var clock, startDate, lastDate;
+  var personMliliseconds = 0;
+  var costPerMilisecond = 0.015 / 1000;
+
+  function startClock() {
+    startDate = lastDate = new Date();
+    clock = setInterval(counter, 10);
+  }
+
+  function counter() {
+    var now = new Date();
+    updatePersonMiliseconds(now);
+    displayCost();
+    displayElapsedTime(now);
+    lastDate = now;
+  }
+
+  function peepsCount() {
+    return $('#peeps li').length;
+  }
+
+  function updatePersonMiliseconds(now) {
+    personMliliseconds += (now - lastDate) * peepsCount();
+  }
+
+  function totalCost() {
+    return (personMliliseconds * costPerMilisecond).toFixed(2);
+  }
+
+  function displayCost() {
+    $('#dollarValue').text(totalCost());
+  }
+
+  function pluralize(number, label) {
+    var string = number + ' ' + label;
+    if (number !== 1) { string += 's'; }
+    return string;
+  }
+
+  function displayElapsedTime(time) {
+    var seconds = Math.floor((time - startDate) / 1000);
+    var totalMinutes = Math.floor(seconds / 60);
+    var hours = Math.floor(totalMinutes / 60);
+    var minutes = totalMinutes % 60;
+
+    var elapsed = [];
+    if (hours)   { elapsed.push(pluralize(hours, 'hour')); }
+    if (minutes) { elapsed.push(pluralize(minutes, 'minute')); }
+
+    $('#elapsedTime').text(elapsed.join(' ') || 'just started');
+  }
+
+  $('#plus').click(function(){
+    $('#peeps').append('<li/>');
+    if (!clock) { startClock(); }
+  });
+
+  $('#minus').click(function(){
+    if (peepsCount()) {
+      $('#peeps li').last().remove();
+    }
+  });
+
+});
